9447cb9a4ff9341d6edbd89c1b8f9cd14ded44ac
michaels90up <michael1990spw@gmail.com>
2022-10-04 21:58:44 +0200

    Update code

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,4 @@
 import Head from 'next/head';
-import Image from 'next/image';
-
-// import zebra from '../public/zebra.jpeg';
 
 export default function Home() {
   return (
@@ -14,42 +11,7 @@ export default function Home() {
         />
       </Head>
 
-      <div>Home</div>
-
-      {/*
-        The Next.js Image component will perform
-        some optimizations such as:
-        - Blocking the space on the page for the image
-          before it loads (to reduce shift of content)
-        - Image optimization (reduction in quality to
-          deliver images faster)
-        - etc
-      */}
-      <Image
-        src="/zebra.jpeg"
-        width="400"
-        height="400"
-        alt="Zebra with meme sunglasses saying 'u wot m8' with a elftroll in the foreground"
-      />
-
-      {/*
-        This is a way of avoiding having to find
-        the width and height and writing them
-        manually in your JSX
-      */}
-      <Image
-        src={zebra}
-        alt="Zebra with meme sunglasses saying 'u wot m8' with a elftroll in the foreground"
-      />
-
-      {/*
-        You can also use the normal img
-        tag if you do not want these optimizations
-      */}
-      <img
-        src="/zebra.jpeg"
-        alt="Zebra with meme sunglasses saying 'u wot m8' with a elftroll in the foreground"
-      />
+      <div>Sculptures for everyone</div>
     </>
   );
 }
